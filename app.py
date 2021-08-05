f7bc9f2eaf7dab1e2372bb80ae1ab6ee80f83a22
Filipe Zaidan <felipezaidan10@gmail.com>
2021-08-05 15:35:33 -0300

    implement admin lte

--- app.py
+++ app.py
@@ -1,20 +1,30 @@
 from flask import Flask, render_template
-import datetime
+from config import bd
+from controller.usuario import usuario_blueprint
 
-TEMPLATES = './templates'
+TEMPLATES = './view'
 STATIC = './static'
 
-app = Flask(__name__, template_folder=TEMPLATES, static_folder=STATIC)
+app = Flask(__name__, static_url_path='', template_folder=TEMPLATES, static_folder=STATIC)
+app.register_blueprint(usuario_blueprint)
+
+# Configuração do Banco de Dados
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///./dados.db'
+bd.init_app(app)
+
+with app.app_context():
+    bd.create_all()
 
 @app.route('/')
 def helloWorld():
-    return render_template('home.html')
+    return render_template('login.html')
 
-@app.route('/home')
+@app.route('/login', methods=['POST'])
+def login():
+    return render_template('index.html')
+
+@app.route('/register')
 def home():
-    data = datetime.datetime.now()
-    usuarios = ['Filipe Zaidan', 'David Glauber', 'Rodrigo Cézar']
-    mostrarUsuarios = True
-    return render_template('home.html', dataAtual=data, usuarios=usuarios, mostrarUsuarios=mostrarUsuarios)
+    return render_template('register.html')
 
 #app.run(host='0.0.0.0', port=5000)
